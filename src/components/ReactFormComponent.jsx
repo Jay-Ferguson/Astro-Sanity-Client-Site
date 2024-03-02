805ea94604d70ed23188783c47b52777700d64ed
Jay-Ferguson <jay_ferguson_15@hotmail.com>
2024-03-02 10:34:39 -0500

    added react form component & added to the main layout

--- src/components/ReactFormComponent.jsx
+++ src/components/ReactFormComponent.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+
+function UpladForm() {
+  const [email, setEmail] = useState("");
+  const [image, setImage] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+}
 
 function ReactFormComponent() {
-  return <div>ReactFormComponent</div>;
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <label htmlFor="image">Image:</label>
+      <input
+        type="file"
+        id="image"
+        accept="image/*"
+        onChange={(e) => setImage(e.target.files[0])}
+      />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default ReactFormComponent;
