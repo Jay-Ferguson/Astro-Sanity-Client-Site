bbae0681fe72868f0c2185a99c04367fce416866
Jay-Ferguson <jay_ferguson_15@hotmail.com>
2024-03-11 19:37:35 -0400

    added styles to main layout

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,18 +1,34 @@
-import {useSanityClient} from 'astro-sanity'
+import { useSanityClient } from "astro-sanity";
 
 export async function getAllPosts() {
-     const client = useSanityClient()
-     const query = '*[_type == "post"]'
-     const params = {}
-     const posts = await client.fetch(query, params)
-     return posts;
+  const client = useSanityClient();
+  const query = '*[_type == "post"]';
+  const params = {};
+  const posts = await client.fetch(query, params);
+  return posts;
 }
 
+export async function getProjects() {
+  const client = useSanityClient();
+  const query = '*[_type == "projects"]';
+  const params = {};
+  const projects = await client.fetch(query, params);
+  return projects;
+}
 
-export async function getProjects(){
-    const client = useSanityClient()
-    const query = '*[_type == "projects"]'
-    const params = {}
-    const projects = await client.fetch(query, params)
-    return projects
+export async function sliderImages() {
+  const sliderQuery = '*[_type == "slider"]';
+  const sliderParams = {};
+  const sliderImages = await useSanityClient().fetch(sliderQuery, sliderParams);
+  return sliderImages;
+}
+
+export async function Categories() {
+  const categoryQuery = '*[_type == "category"]';
+  const categoryParams = {};
+  const categories = await useSanityClient().fetch(
+    categoryQuery,
+    categoryParams
+  );
+  return categories;
 }
