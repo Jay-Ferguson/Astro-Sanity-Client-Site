7c0a1927d8ae6c5850a9a86d885742a82a695a86
Jay-Ferguson <jay_ferguson_15@hotmail.com>
2024-03-18 14:40:50 -0400

    added slider component

---
+++ src/components/HeaderSlider.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Image } from "astro:assets";
+import { getAllsliderImages } from "../lib/api";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {KayboardNavigation, Controller, A11y} from "swiper";
+
+import "swiper/css";
+
+const headerImages = await getAllsliderImages();
+
+function HeaderSlider(props) {
+  return (
+    <Swiper>
+      slidesPerView={1}
+      onSwiper={(swiper)=> }
+      <div class="slider-container" data-swiper-autoplay="2000" 
+      data-swiper-crossFade='true' 
+      data-swiper-autoplay="true">
+        {headerImages.map((headerImage) => {
+          return (
+            <SwiperSlide>
+              <Image src={headerImage.image} alt={headerImage.alt} />;
+            </SwiperSlide>
+          );
+        })}
+      </div>
+    </Swiper>
+  );
+}
+
+HeaderSlider.propTypes = {
+  image: propTypes.string,
+  alt: propTypes.string,
+};
+
+<style></style>;
+
+export default HeaderSlider;
