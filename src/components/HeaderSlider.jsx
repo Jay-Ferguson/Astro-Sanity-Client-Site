3e95ba81c78e966ee37436a793e745e421f104ca
Jay-Ferguson <jay_ferguson_15@hotmail.com>
2024-03-19 09:49:44 -0400

    changed swiper slide to v10 webcomponents

--- src/components/HeaderSlider.jsx
+++ src/components/HeaderSlider.jsx
@@ -1,36 +1,49 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Image } from "astro:assets";
+import { urlForImage } from "../lib/urlForImage";
 import { getAllsliderImages } from "../lib/api";
-import { Swiper, SwiperSlide } from "swiper/react";
-import {KayboardNavigation, Controller, A11y} from "swiper";
+import Swiper from "swiper";
+import {
+  Navigation,
+  Pagination,
+  Autoplay,
+  KayboardNavigation,
+} from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/autoplay";
 import "swiper/css/controller";
 import "swiper/css/effect-fade";
 
-
-
 const headerImages = await getAllsliderImages();
+
+const swiper = document.querySelector(".swiper").swiper;
 
 function HeaderSlider(props) {
   return (
-    <Swiper>
+    <swiper-container>
       slidesPerView={1}
-      onSwiper={(swiper)=> }
-      <div class="slider-container" data-swiper-autoplay="2000" 
-      data-swiper-crossFade='true' 
-      data-swiper-autoplay="true">
-        {headerImages.map((headerImage) => {
-          return (
-            <SwiperSlide>
-              <Image src={headerImage.image} alt={headerImage.alt} />;
-            </SwiperSlide>
-          );
-        })}
+      <div className="swiper">
+        <div
+          class="swiper-wrapper"
+          data-swiper-autoplay="2000"
+          data-swiper-crossFade="true"
+        >
+          {headerImages.map((headerImage) => {
+            return (
+              <SwiperSlide>
+                <Image
+                  src={urlForImage(headerImage.image)}
+                  alt={headerImage.alt}
+                />
+                ;
+              </SwiperSlide>
+            );
+          })}
+        </div>
       </div>
-    </Swiper>
+    </swiper-container>
   );
 }
 
@@ -38,7 +51,5 @@ HeaderSlider.propTypes = {
   image: propTypes.string,
   alt: propTypes.string,
 };
-
-<style></style>;
 
 export default HeaderSlider;
