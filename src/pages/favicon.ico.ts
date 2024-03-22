e0060b4d0f312ba2ef0d2c3842172990c27f60bd
Jay-Ferguson <jay_ferguson_15@hotmail.com>
2024-03-22 14:13:14 -0400

    added footer and heading menu to layout file

--- src/pages/favicon.ico.ts
+++ src/pages/favicon.ico.ts
@@ -1,18 +1,16 @@
 import type { APIRoute } from "astro";
 import sharp from "sharp";
-import ico from "sharp-ico";
 import path from "node:path";
 
 // relative to project root
-const faviconSrc = path.resolve("src/images/favicon.png");
+const faviconSrc = path.resolve("src/local-images/favicon.png");
 
 export const GET: APIRoute = async () => {
   // resize to 32px PNG
   const buffer = await sharp(faviconSrc).resize(32).toFormat("png").toBuffer();
   // generate ico
-  const icoBuffer = ico.encode([buffer]);
 
-  return new Response(icoBuffer, {
+  return new Response(buffer, {
     headers: { "Content-Type": "image/x-icon" },
   });
 };
