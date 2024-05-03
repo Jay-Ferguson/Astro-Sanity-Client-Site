d14e5c31e1d35bd5847e5f35c2e25ff53bc8cba8
Jay-Ferguson <jay_ferguson_15@hotmail.com>
2024-05-03 14:21:53 -0400

    added lottie integration for 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { sanityIntegration } from "@sanity/astro";
 import react from "@astrojs/react";
 import { defineConfig } from 'astro/config';
 import {imageService} from '@unpic/astro/service';
+import lottie from 'astro-integration-lottie'
 import {loadEnv} from 'vite';
 
 // const projectId = loadEnv(process.env.PROJECT_ID, process.cwd(), "");
@@ -19,7 +20,7 @@ export default defineConfig({
       
     })
   },
-  integrations: [react(), sanityIntegration({
+  integrations: [react(), lottie(), sanityIntegration({
     projectId: '9gy3ebd9',
     dataset: "production",
     //set 
