ec0afd9a8b94c597b3a7d19f1688399dd0fa333b
Jay-Ferguson <jay_ferguson_15@hotmail.com>
2024-05-17 11:42:09 -0400

    added astro-icon to config to add all favicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,29 +6,41 @@ import react from "@astrojs/react";
 import { defineConfig } from 'astro/config';
 import {imageService} from '@unpic/astro/service';
 import lottie from 'astro-integration-lottie'
+import favicons from 'astro-favicons';
 import {loadEnv} from 'vite';
 
 // const projectId = loadEnv(process.env.PROJECT_ID, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://warnockcabinetry.com',
-  output:'static',
-  prefetch:true,
-  base:'/',
-  image:{
+  site: "https://warnockcabinetry.com",
+  output: "static",
+  prefetch: true,
+  base: "/",
+  lang: "en-CA",
+  image: {
     service: imageService({
-      fallbackService: 'netlify',
-      placeholder: 'blurhash',
-      layout:'constrained',
-      
-    })
+      fallbackService: "netlify",
+      placeholder: "blurhash",
+      layout: "constrained",
+    }),
   },
-  integrations: [sitemap(),robotsTxt(),react(), lottie(), sanityIntegration({
-    projectId: '9gy3ebd9',
-    dataset: "production",
-    //set 
-    useCdn: false
-  })
-]
+  integrations: [
+    sitemap(),
+    robotsTxt(),
+    favicons({
+      masterPicture: "/src/images/warnocklogo.svg",
+      emitAssets: true,
+      orientation: "any",
+      appleStatusBarStyle: "black-translucent",
+    }),
+    react(),
+    lottie(),
+    sanityIntegration({
+      projectId: "9gy3ebd9",
+      dataset: "production",
+      //set
+      useCdn: false,
+    }),
+  ],
 });
