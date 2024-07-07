8022e329fab44158d3ac8f92bcb01207198aea61
Jay-Ferguson <jay_ferguson_15@hotmail.com>
2024-07-07 08:42:33 -0400

    changed webhook value for netlify content changes

--- sanity/sanity.config.js
+++ sanity/sanity.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     visionTool(),
     media(),
     umamiTool({
-      title: 'analytics',
+      title: 'Analytics',
       url: 'https://cloud.umami.is/share/bndsco0iKnm0c7av/www.warnockcabinetry.ca',
       //example: https://eu.umami.is/share/XXXX/XXXX
     }),
@@ -32,18 +32,18 @@ export default defineConfig({
     }),
     dashboardTool({
       name: 'dashboard',
-      title: 'Deploy',
+      title: 'Dashboard Tools',
       widgets: [
         netlifyWidget({
           layout: 'medium',
           title: 'My Netlify deploys',
-          description: 'deploys website with all published changes',
+          description: 'deploys Warnock Cabinetry website with all published changes',
           sites: [
             {
               title: 'Warnock Website Deploy',
               siteId: 'cdeaf534-e38d-47f0-8af6-c9302cfc8705',
               apiId: 'bb377697-7601-4d2f-8ba9-9b10ff19ac51',
-              buildHookId: 'https://api.netlify.com/build_hooks/6689be2ee539312d68508f64',
+              buildHookId: '6689be2ee539312d68508f64',
               name: 'webdeploy',
               url: 'https://www.warnockcabinetry.ca',
               branch: 'main',
